c905c7db4383b3f6e9d846eb4df95a025b2e6447
praveen <spraveenitpro@gmail.com>
2019-06-19 23:56:20 -0700

    Added backgroud image

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  View
+} from "react-native";
+import getImageForWeather from "./utils/getImageForWeather";
+import SearchInput from "./components/SearchInput";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={[styles.largeText, styles.textStyle]}>San francisco</Text>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              San francisco
+            </Text>
 
-        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
 
-        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-      </View>
+            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -18,9 +40,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#34495E"
   },
   textStyle: {
     textAlign: "center",
@@ -32,5 +52,21 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  },
+  imageContainer: {
+    flex: 1
+  },
+
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
+    paddingHorizontal: 20
   }
 });
